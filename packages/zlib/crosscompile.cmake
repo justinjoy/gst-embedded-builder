55ff6940a74e9388a407db228aa520698d129d83
Justin Joy <justin.joy.9to5@gmail.com>
2013-03-11 21:47:15 +0900

    added toolchain description for zlib

--- packages/zlib/crosscompile.cmake
+++ packages/zlib/crosscompile.cmake
@@ -4,6 +4,10 @@ IF ( "${PLATFORM_CHIP}" STREQUAL "lg1154" )
 	SET(CMAKE_C_COMPILER arm-lg115x-linux-gnueabi-gcc)
 ENDIF ()
 
+IF ( "${TARGET_PLATFORM_NAME}" STREQUAL "webos-h13" )
+	SET(CMAKE_C_COMPILER arm-lg115x-linux-gnueabi-gcc)
+ENDIF ()
 
-MESSAGE ( STATUS "selected compiler ==> " ${PLATFORM_CHIP} ":" ${CMAKE_C_COMPILER})
+
+MESSAGE ( STATUS "selected compiler ==> " ${PLATFORM_CHIP} ":" ${TARGET_PLATFORM_NAME} ":" ${CMAKE_C_COMPILER})
 
